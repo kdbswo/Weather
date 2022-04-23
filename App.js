a76f7a298f10c51967b2b407a1af23d23bea75c3
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-04-23 14:51:39 +0900

    1.0
    stylesheet.create

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello! I made a RN App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 28,
+    color: 'red'
+  }
 });
