22beda15f4c90944e3f572284d0be14021ec65f5
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-04-25 00:12:01 +0900

    weather0.0

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 
-import { View } from 'react-native';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  return (
-    <View style={{flex:1, flexDirection: 'row'}}>
-      <View style={{flex: 1, backgroundColor: "red"}}></View>
-      <View style={{flex: 1, backgroundColor: "blue"}}></View>
-      <View style={{flex: 1, backgroundColor: "teal"}}></View>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "yellow" },
+});
